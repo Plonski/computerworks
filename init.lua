eec6a7d58943e282ae88c4fc91bf7b9945ad447c
plonski <tplonski@villanova.edu>
2017-05-29 23:26:55 -0400

    Added formspec for computer

--- init.lua
+++ init.lua
@@ -1,19 +1,48 @@
+local computer = minetest.get_content_id("default:chest")
+
+local computer_formspec =
+	"size[8,9]"..
+	--default.gui_bg..
+	--default.gui_bg_img..
+	--default.gui_slots..
+	"list[current_name;main;0,0.3;8,4;]"..
+	"list[current_player;main;0,4.85;8,1;]"..
+	"list[current_player;main;0,6.08;8,3;8]"..
+        "button[2,2;2,2;name;Load disk]"
+--default.get_hotbar_bg(0,4.85)
+
+
 minetest.register_node('computerworks:farming', {
     description = 'computer',
     titles = {'picture.png'},
     group = {snappy=1, oddly_breakable_by_hand=2},
 
-    after_place_node = function(pos, placer)
-         local meta = minetest.get_meta(pos)
-         meta:set_string("formspec",
-                         "size[30,30]",
-                         "label[1,1; This is shown on right click]"..
-                         "field[1,2;2,1;x;x;]")
-         end,
+
+    on_construct = function(pos)
+	local meta = minetest.get_meta(pos)
+	meta:set_string("formspec", computer_formspec)
+	meta:set_string("infotext", "Computer")
+	local inv = meta:get_inventory()
+	inv:set_size("main", 1*1)
+    end,
+
+
     on_receive_fields = function(pos, formname, fields, player)
          if (fields.quit) then return end
          print(fields.x)
-    end  
+    end,  
+
+    on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+        local meta = minetest.get_meta(pos)
+        meta:set_string("formspec",
+                         "size[15, 15]",
+                         "label[1,1; This is shown on right click]"..
+                         "field[1,2;2,1;x;x;]")
+
+    end
+
+
+         
 
 })
 
